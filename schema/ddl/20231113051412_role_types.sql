7ecf71b5ab702d59fa34992a6e3453867f19712e
aizawa <ikiatawazia@outlook.jp>
2024-03-14 04:41:27 +0900

    fix: role_types.sql

--- schema/ddl/20231113051412_role_types.sql
+++ schema/ddl/20231113051412_role_types.sql
@@ -1,9 +1,17 @@
 -- +goose Up
 -- +goose StatementBegin
 CREATE TABLE IF NOT EXISTS role_types (
-    id SERIAL PRIMARY KEY,
-    type VARCHAR(255) NOT NULL
+    "id" SERIAL PRIMARY KEY,
+    "type" VARCHAR(255) NOT NULL,
+    "created_at" timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "deleted_at" TIMESTAMP DEFAULT NULL,
 );
+CREATE INDEX idx_role_types_created_at ON role_types (created_at);
+
+COMMENT ON COLUMN role_types.id IS 'ロールID';
+COMMENT ON COLUMN role_types.id IS 'ロール名';
+
 -- +goose StatementEnd
 
 -- +goose Down
