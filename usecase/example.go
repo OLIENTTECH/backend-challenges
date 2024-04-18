d6fa998c79918b9d2eefe3c41f11c2ac0eae066b
aizawa <ikiatawazia@outlook.jp>
2024-04-18 19:48:15 +0900

    fix: comment out the example

--- usecase/example.go
+++ usecase/example.go
@@ -1,81 +1,81 @@
 package usecase
 
-import (
-	"context"
+// import (
+// 	"context"
 
-	"github.com/OLIENTTECH/backend-challenges/domain/model"
-	"github.com/OLIENTTECH/backend-challenges/infrastructure/datastore"
-	rdb "github.com/OLIENTTECH/backend-challenges/infrastructure/external/db/postgres"
-	"github.com/OLIENTTECH/backend-challenges/internal/cerror"
-	"github.com/OLIENTTECH/backend-challenges/pkg/log"
-	"github.com/OLIENTTECH/backend-challenges/usecase/input"
-	"github.com/OLIENTTECH/backend-challenges/usecase/output"
-)
+// 	"github.com/OLIENTTECH/backend-challenges/domain/model"
+// 	"github.com/OLIENTTECH/backend-challenges/infrastructure/datastore"
+// 	rdb "github.com/OLIENTTECH/backend-challenges/infrastructure/external/db/postgres"
+// 	"github.com/OLIENTTECH/backend-challenges/internal/cerror"
+// 	"github.com/OLIENTTECH/backend-challenges/pkg/log"
+// 	"github.com/OLIENTTECH/backend-challenges/usecase/input"
+// 	"github.com/OLIENTTECH/backend-challenges/usecase/output"
+// )
 
-type User interface {
-	Get(ctx context.Context, input *input.GetUserDTO) (*output.UserDTO, error)
-	List(ctx context.Context) (*output.ListUsers, error)
-	Create(ctx context.Context, input *input.CreateUserDTO) (*output.UserDTO, error)
-}
+// type User interface {
+// 	Get(ctx context.Context, input *input.GetUserDTO) (*output.UserDTO, error)
+// 	List(ctx context.Context) (*output.ListUsers, error)
+// 	Create(ctx context.Context, input *input.CreateUserDTO) (*output.UserDTO, error)
+// }
 
-type userUsecase struct {
-	tx     rdb.TxManager
-	ds     datastore.DataStore
-	logger *log.Logger
-}
+// type userUsecase struct {
+// 	tx     rdb.TxManager
+// 	ds     datastore.DataStore
+// 	logger *log.Logger
+// }
 
-func NewUserUsecase(tx rdb.TxManager, ds datastore.DataStore, logger *log.Logger) User {
-	return &userUsecase{
-		tx:     tx,
-		ds:     ds,
-		logger: logger,
-	}
-}
+// func NewUserUsecase(tx rdb.TxManager, ds datastore.DataStore, logger *log.Logger) User {
+// 	return &userUsecase{
+// 		tx:     tx,
+// 		ds:     ds,
+// 		logger: logger,
+// 	}
+// }
 
-func (u *userUsecase) Get(ctx context.Context, input *input.GetUserDTO) (*output.UserDTO, error) {
-	user, err := u.ds.User().Get(ctx, input.UserID)
-	if err != nil {
-		u.logger.Warn("usecase: failed to get user", log.Ferror(err))
+// func (u *userUsecase) Get(ctx context.Context, input *input.GetUserDTO) (*output.UserDTO, error) {
+// 	user, err := u.ds.User().Get(ctx, input.UserID)
+// 	if err != nil {
+// 		u.logger.Warn("usecase: failed to get user", log.Ferror(err))
 
-		return nil, cerror.Wrap(err, "usecase")
-	}
+// 		return nil, cerror.Wrap(err, "usecase")
+// 	}
 
-	userDTO := user.ToDTO()
+// 	userDTO := user.ToDTO()
 
-	return userDTO, nil
-}
+// 	return userDTO, nil
+// }
 
-func (u *userUsecase) List(ctx context.Context) (*output.ListUsers, error) {
-	users, err := u.ds.User().List(ctx)
-	if err != nil {
-		u.logger.Warn("usecase: failed to get users", log.Ferror(err))
+// func (u *userUsecase) List(ctx context.Context) (*output.ListUsers, error) {
+// 	users, err := u.ds.User().List(ctx)
+// 	if err != nil {
+// 		u.logger.Warn("usecase: failed to get users", log.Ferror(err))
 
-		return nil, cerror.Wrap(err, "usecase")
-	}
+// 		return nil, cerror.Wrap(err, "usecase")
+// 	}
 
-	userList := make([]*output.UserDTO, 0, len(users))
-	for _, user := range users {
-		userList = append(userList, user.ToDTO())
-	}
+// 	userList := make([]*output.UserDTO, 0, len(users))
+// 	for _, user := range users {
+// 		userList = append(userList, user.ToDTO())
+// 	}
 
-	return &output.ListUsers{
-		Users: userList,
-	}, nil
-}
+// 	return &output.ListUsers{
+// 		Users: userList,
+// 	}, nil
+// }
 
-func (u *userUsecase) Create(ctx context.Context, input *input.CreateUserDTO) (*output.UserDTO, error) {
-	user := model.NewUser(
-		input.LoginID,
-		input.Password,
-		input.FamilyName,
-		input.GivenName,
-		input.IsShopManager,
-	)
-	err := u.ds.User().Create(ctx, user)
-	if err != nil {
-		return nil, cerror.Wrap(err, "usecase")
-	}
-	userDTO := user.ToDTO()
+// func (u *userUsecase) Create(ctx context.Context, input *input.CreateUserDTO) (*output.UserDTO, error) {
+// 	user := model.NewUser(
+// 		input.LoginID,
+// 		input.Password,
+// 		input.FamilyName,
+// 		input.GivenName,
+// 		input.IsShopManager,
+// 	)
+// 	err := u.ds.User().Create(ctx, user)
+// 	if err != nil {
+// 		return nil, cerror.Wrap(err, "usecase")
+// 	}
+// 	userDTO := user.ToDTO()
 
-	return userDTO, nil
-}
+// 	return userDTO, nil
+// }
