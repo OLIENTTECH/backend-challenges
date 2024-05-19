67527ec74ccb30df6b2abdf1656899260f523fa1
aizawa <ikiatawazia@outlook.jp>
2024-05-19 19:53:19 +0900

    feat: automatic password generation

---
+++ pkg/password/password.go
@@ -0,0 +1,21 @@
+package password
+
+import (
+    "crypto/rand"
+	"github.com/OLIENTTECH/backend-challenges/pkg/log"
+)
+
+func MakeRandomStr() string {
+    const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+    b := make([]byte, 8)
+    if _, err := rand.Read(b); err != nil {
+        log.Error("unexpected error...", log.Ferror(err))
+    }
+
+    var result string
+    for _, v := range b {
+        result += string(letters[int(v)%len(letters)])
+    }
+    return result
+}
